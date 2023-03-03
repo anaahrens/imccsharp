50e9289f2199b6ea13b74faba4ae28ef87bbcf4c
Ana Paula Ahrens <105805241+anaahrens@users.noreply.github.com>
2023-03-03 01:01:45 +0100

    Add files via upload

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Exercicio21
+{
+    internal class Program
+    {
+        static void Main (string[ ] args)
+        {
+
+            Console.WriteLine("Digite seu peso: ");
+            double peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite sua altura: ");
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double imc = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: ", imc);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Abaixo do peso.");
+            }
+            else if (imc < 24.9) 
+            {
+                Console.WriteLine("Peso normal.");
+            }
+            else if (imc < 29.9)
+            {
+                Console.WriteLine("Sobrepeso.");
+            }
+            else if (imc < 34.9)
+            {
+                Console.WriteLine("Obesidade grau 1");
+            }
+            else if (imc < 39.9)
+            {
+                Console.WriteLine("Obesidade grau 2");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade grau 3");
+            }
+    }
+    }
+}
